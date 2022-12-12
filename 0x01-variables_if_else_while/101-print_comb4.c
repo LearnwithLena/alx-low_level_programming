d38d6d2ffe95c13fc7c4f7c72197ac190a73e412
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-12 13:34:50 -0800

    Print all combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Print three numbers combo
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hund;
+	int tens;
+	int unit;
+
+	for (hund = '0'; hund <= '9'; hund++)
+	{
+		for (tens = hund + 1; tens <= '9'; tens++)
+		{
+			for (unit = hund + 2; unit <= '9'; unit++)
+			{
+				putchar(hund);
+				putchar(tens);
+				putchar(unit);
+
+				if (hund != '7' || tens != '8' || unit != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
