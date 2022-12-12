5191c4b4959ff9e09ef96b15fe3bd5762648286d
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-12 12:36:01 -0800

    Print combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char num = '0';
+
+	/* Loops through the numbers as strings */
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
