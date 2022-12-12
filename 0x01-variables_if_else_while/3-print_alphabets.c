4ebe57acfd8e13faf6685fca1819e02939fa645b
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-11 22:06:29 -0800

    Prints alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints upper and lowercase alphabets with putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char u_alphabets[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char l_alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	/* Prints lower case */
+	for (i = 0; i < 26; i++)
+	{
+		putchar(l_alphabets[i]);
+	}
+	
+	/* Prints upper case */
+	for (i = 0; i < 26; i++)
+	{
+		putchar(u_alphabets[i]);
+	}
+
+	/* Print new line */
+	putchar('\n');
+
+	return (0);
+}
