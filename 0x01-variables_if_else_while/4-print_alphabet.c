46dd4bdbebfab0a0cdaf4424460e4475ad49c474
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-11 22:23:55 -0800

    Print some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/** 
+ * main - Prints lower case alphabets except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpa[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	/* Print lower case alphabets */
+
+	for (i = 0; i < 26; i++)
+	{
+		/* Checks if the letter is q or e */
+		if ((putchar(alpha[i]) != 'q') && (putchar(alpha[i]) != 'e' ))
+		{
+			putchar(alpha[i]);
+		}
+		
+		/* Print new line */
+		putchar('\n');
+	}
+}
