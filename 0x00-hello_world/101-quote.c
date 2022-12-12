288608f2b9bc90cb2bc797f72f108e1e2a1804cb
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-12 14:01:09 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print a sentence without printf or puts
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i = 0
+	char sentence[33] = "and that piece of art is useful\" "
+	char authour[25] = "- Dora Korpar, 2015-10-19"
+
+	for (i = 0; i <= 33; i++)
+	{
+		putchar(sentence[i]);
+	}
+	for (i = 0; i <= 25; i++)
+	{
+		putchar(authour[i]);
+	}
+	putchar('\n');
+	return (0);
+}
