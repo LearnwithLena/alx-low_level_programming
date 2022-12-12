c3cf9f9f535749f2cb850f42781d24510b22675d
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-11 23:48:30 -0800

    Hexadecimal characters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,15 +11,30 @@ int main(void)
 	char num = '0';
 	char alpha = 'a';
 
-	for(num = '0'; num <= '9'; num++)
-       	{
+	/* Print numbers 0 to 9 as strings */
+	for (num = '0'; num <= '9'; num++)
+	{
 		putchar(num);
 	}
+	
+	/*
+	 * While (num <= '9')
+	 * {
+	 * putchar(num);
+	 * num++
+	 */
 
-	for(alpha = 'a'; alpha <= 'f'; alpha++)
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
 	{
 		putchar(alpha);
 	}
+	/*
+	 * While (alpha <= 'f')
+	 * {
+	 * putchar(alpha);
+	 * alpha++
+	 * }
+	*/
 	putchar('\n');
 	return (0);
 }
