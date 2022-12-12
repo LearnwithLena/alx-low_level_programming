ee3a411f66c273815e6625c2c3582e9ea27e1eac
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-11 23:11:29 -0800

    Print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/** 
+ * main - Prints numbers less than ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = '0'
+
+	while (i < '10')
+	{
+		putchar(i);
+		i++
+	}
+	putchar('\n')
+	
+	return (0)
+}
