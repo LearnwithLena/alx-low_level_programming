2987910a982dc2e1160438bd49f9bd400505911f
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-14 08:56:45 -0800

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,14 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	last_digit = num % 10;;
+	last_digit = num % 10;
 
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
 	}
 
-	_putchar((last_digit + '0');
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
