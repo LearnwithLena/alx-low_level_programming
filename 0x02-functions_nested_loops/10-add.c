3433d61124d742728deef7e414f6cbc19aedb562
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-14 11:11:22 -0800

    Sum of two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,22 +12,6 @@ int add(int a, int b)
 	int sum;
 
 	sum = a + b;
+	return (sum);
 
-	/*
-	 * If sum is greater than 9, putchar prints only 0 - 9
-	 * The double digits have to be printed one after the other.
-	 * If sum <= 9, then it can be printed.
-	 */
-
-	if (sum > 9)
-	{
-		_putchar('0' + (sum / 10));
-		_putchar('0' + (sum % 10));
-	}
-	else
-	{
-		_putchar('0' + sum);
-	}
-	_putchar('\n');
-	return 0;
 }
