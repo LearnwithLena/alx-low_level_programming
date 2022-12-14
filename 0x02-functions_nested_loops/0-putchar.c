52465060f4826edb51d312110d36ca251c024e4f
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-13 18:38:02 -0800

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char sentence [] = "_putchar";
+	while (i < strlen(sentence))
+	{
+		putchar(sentence[i]);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
