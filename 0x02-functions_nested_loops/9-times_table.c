5bf3a7029562f2e66e011351fab92cd650abccc3
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-14 09:33:48 -0800

    Nine times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 	int column;
 	int row;
 
-	for (column = '0'; column < '10'; column++)
+	for (column = '0'; column <= '9'; column++)
 	{
-		for (row = '0''; row < '10''; row++)
+		for (row = '0'; row <= '9'; row++)
 		{
 			putchar(column * row);
 			putchar(',');
