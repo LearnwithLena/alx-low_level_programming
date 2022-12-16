911bdef3da05212fe993a1a02529b913dc2759a5
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-16 15:05:56 -0800

    Prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print largest num = 6128525143
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	unsigned long num = 612852475143;
+	unsigned long div = 2;
+
+	while (div < num)
+	{
+		if (num % div == 0)
+		{
+			num /= divisor;
+			div = 2;
+		}
+		else
+		{
+			div++
+		}
+	}
+	printf("%lu\n", num);
+	return (0);
+}
