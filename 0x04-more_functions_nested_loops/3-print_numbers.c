70a02fefb7c09367aac4c01a97f5eec3d68dad12
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-15 16:29:24 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - Prints numbers 1 t0 9
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
