3cafd0d8f1c3ffcf1e88a057e04fb240346c3ea7
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-16 14:39:36 -0800

    Print

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,17 +12,27 @@ void print_triangle(int size)
 	int space;
 	int column;
 
-	for (row = 0; row < size; row++)
+	if (size <= 0)
 	{
-		for (space = 0; space < size - row - 1; space++)
-		{
-			_putchar(' ');
-		}
-
-		for (column = 0; column <= row; column++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
 	}
+	else
+	{
+
+
+		for (row = 0; row < size; row++)
+		{
+			for (space = 0; space < size - row - 1; space++)
+			{
+				_putchar(' ');
+			}
+
+			for (column = 0; column <= row; column++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
 }
+
