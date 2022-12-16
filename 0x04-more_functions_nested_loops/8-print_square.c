b8e58dec460b552fd632cf5a7593a758b855cd87
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-15 18:24:46 -0800

    New

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,15 +15,12 @@ void print_square(int size)
 	{
 		for (column = 0; column < size; column++)
 		{
-			if (size > 0)
-			{
-				_putchar('#');
-			}
-			else if (size <= 0)
+			if (size <= 0)
 			{
 				_putchar('\n');
 			}
-
+			
+			_putchar('#');
 		}
 		_putchar('\n');
 	}
