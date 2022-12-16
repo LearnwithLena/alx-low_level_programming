c03f3bc2f41457aa4e024788cfed9f1ec386a3d3
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-15 17:06:35 -0800

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints more numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 11; row++)
+	{
+		for (column = 0; column < 15; column++)
+		{
+			if (column >= 10)
+			{
+				_putchar((column / 10) + '0'); /*First digit */
+			}
+				_putchar(column % 10 + '0'); /*Last digit */
+		}
+		_putchar('\n')
+	}
+}
