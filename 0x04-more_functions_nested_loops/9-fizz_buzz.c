2a29f0a9361fcf73bb7638c479bc1ab69e472361
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-27 11:01:51 -0800

    Edit loop functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,21 @@ int main(void)
 {
 	int i;
 
-	i = 1;
-	printf("%d", i);
-
-	for (i = 2; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
