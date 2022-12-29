395cc483cab3c0362014a713c545ebf0d6938196
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-29 09:59:14 -0800

    Puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - print every other character beginning from the first
+ *
+ * @str: The character to print
+ *
+ * Return: None
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str); /* length of string */
+
+	/* Print first number but skip everyt other */
+	for (i = 0; i < len; i = i + 2)
+	{
+
+		puts(str[i]);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -18,4 +18,7 @@ void _puts(char *str);
 void print_rev(char *s);
 
 void reve_string(char *s);
+
+void puts2(char *str);
+
 #endif /*MAIN_H*/
