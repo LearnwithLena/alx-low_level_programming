2c4e7d0d4c98778eec721f6f6296892178696874
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-29 12:47:12 -0800

    String to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - Converts string to int
+ *
+ * @s: String to be converted
+ *
+ * Return: The integer
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
