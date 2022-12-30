9d390b0a3c21b7eb353a8b46c9cfa15fd8144c1c
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-30 15:07:53 -0800

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - capitalise words in string
+ *
+ * @s: string
+ *
+ * Return: capitalised string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[0] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32; /* lowercase to uppercase in ASCII */
+		}
+	}
+	return (s);
+}
