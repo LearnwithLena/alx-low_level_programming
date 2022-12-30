ab38cfbe2650ddcff840fdb21afcc87359418932
LearnwithLena <Learnmit.Lena@gmail.com>
2022-12-30 11:20:37 -0800

    Initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ *
+ * @src: source string
+ *
+ * @dest: destination string
+ *
+ * Return: concatenated string
+ */
+
+char *_strcat(chat *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H
+
+/**
+ * Header files for 0x06 tasks
+ */
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+
+
+#endif /*MAIN_H*/
