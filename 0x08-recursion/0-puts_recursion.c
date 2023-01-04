600b7b4459806875c099fd15e114761e5c6b049d
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-04 13:55:05 -0800

    First commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
