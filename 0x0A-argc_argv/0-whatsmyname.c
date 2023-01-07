968a52f556ec03cf8c7532554c1b98c99e929bf0
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-06 17:01:29 -0800

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the programe
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
