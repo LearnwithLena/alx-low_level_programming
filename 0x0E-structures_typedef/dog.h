db7551c1e6495eb83d1ef077ecae05632f6b8819
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-17 04:17:57 -0800

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct for characters of a dog
+ * @name: name of dog
+ * @age: Dogs age
+ * @owner: Owner of the dog
+ *
+ * Descriptio: struct called dog with name, age and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif /* _DOG_H */
