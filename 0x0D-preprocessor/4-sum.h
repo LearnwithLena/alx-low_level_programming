c01b62b8d960777802b6d0ae5ebda51762270aae
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-17 03:33:06 -0800

    Sum macros

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef _SUM_H_
+#define _SUM_H_
+
+/**
+ * File name: 4-sum.h
+ * Description: A header file that contains a macro
+ *		to sum two numbers together
+ * @x: num 1
+ * @y: num 2
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /*_SUM_H_ */
