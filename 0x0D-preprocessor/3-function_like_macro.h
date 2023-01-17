ed3d873bdfd45382786feff0b6ca58c214e349ae
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-17 02:27:45 -0800

    Absolute macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
+
+/**
+ * File name: 3-function_like_macro.h
+ * Description: Macro function that prints the absolute value
+ * @x: number to be used
+ */
+
+#define ABS(x) ((x < 0) ? (x) : (x))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
