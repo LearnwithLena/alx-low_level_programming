93e3a54bcf2a09f6755f6bf1699391e218b5717b
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-17 01:46:25 -0800

    Pi micro

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+#ifndef _PI_H_
+#define _PI_H_
+
+/**
+ * File name: 1-pi.h
+ * Description: Header file PI that defines an abrreviation for 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
