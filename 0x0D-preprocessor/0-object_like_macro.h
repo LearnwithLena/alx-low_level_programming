ef1844ea1150712e20d308b3d5f7533a7f4ea3de
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-17 01:39:01 -0800

    Add file guard

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,12 @@
+#ifndef _OBJECT_LIKE_MICRO_H
+#define _OBJECT_LIKE_MICRO_H
+
+/**
+ * Name of file: 0-0bject_like_micro.h
+ * Description: A header file that defines a micro called SIZE
+ *		Assigns it a value of 1024
+ */
+
 #define SIZE 1024
+
+#endif
