3c9c20e67e99e13d14300fdd10d78315ab0df3a2
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-17 02:07:14 -0800

    Removed argv and argc, added __FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,17 +3,15 @@
 
 /**
  * main - prints the name of the file it was compiled from
- * @argc: Number of command line inputs
- * @argv: Array of inputs
  * Return: 0 if success, prints name of file
  *
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	if (argc)
 	{
-		printf("%s\n", argv[0]);
+		printf("%s\n", __FILE__);
 	}
 	return(0);
 }
