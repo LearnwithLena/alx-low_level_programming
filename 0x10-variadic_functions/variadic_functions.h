d11619d9c76342451aad12dceefdc3cf053ecae3
LearnwithLena <Learnmit.Lena@gmail.com>
2023-01-20 00:21:54 -0800

    Description of struct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,11 @@
 #define _VARIADIC_FUNCTIONS_H_
 
 #include <stdarg.h>
+
 /**
- * name of file: variadic_functions.h
- * Desc: contains protoypes of functions 0x10
+ * struct print - struct to print all characters
+ * @type: indicates the data type
+ * @func: function pointer to function to print data type
  */
 
 typedef struct print
@@ -23,4 +25,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char *const format, ...);
 
-#endif /*_VARIADIC_FUNCTIONS_H_ */ 
+#endif /*_VARIADIC_FUNCTIONS_H_ */
